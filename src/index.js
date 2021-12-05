d9b0e48e0d395dc0e27efaa60227f965c6cb7572
surajverma2587 <surajverma2587@gmail.com>
2021-12-05 21:41:23 +0000

    move middleware

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(clog);
 app.use(routes);
 
