12864c4c87da6a4c9d2e1fbbf2256fcb85da968f
surajverma2587 <surajverma2587@gmail.com>
2021-12-05 21:37:13 +0000

    update path

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const routes = require('./routes');
-const { clog } = require('./middleware/clog');
+const routes = require("./routes");
+const { clog } = require("./middleware/clog");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(clog);
